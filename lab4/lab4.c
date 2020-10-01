1d39c2f98cc2ab41786bc5fa84d71bb4db14e42b
sofli181 <sofli181@olympen1-111.ad.liu.se>
2020-10-01 16:47:59 +0200

    lab4 part 1

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -16,15 +16,71 @@
 #include "LoadTGA.h"
 #include "SpriteLight.h"
 #include "GL_utilities.h"
+#include "VectorUtils3.h"
 
 // L�gg till egna globaler h�r efter behov.
 
 void SpriteBehavior() // Din kod!
 {
+	SpritePtr sp;
+	SpritePtr boid;
+	double avgPosH, avgPosV, tp_h, tp_v;
+	int count;
+	vec3 v;
+
+	GLfloat kMaxdistance = 200;
+	GLfloat kMindistance = 5;
 	// L�gg till din labbkod h�r. Det g�r bra att �ndra var som helst i
 	// koden i �vrigt, men mycket kan samlas h�r. Du kan utg� fr�n den
 	// globala listroten, gSpriteRoot, f�r att kontrollera alla sprites
 	// hastigheter och positioner, eller arbeta fr�n egna globaler.
+
+	// Loop though all sprites. (Several loops in real engine.)
+	sp = gSpriteRoot;
+	do
+	{
+		// check all neighbours
+		boid = gSpriteRoot;
+
+		avgPosH = sp->position.h;
+		avgPosV = sp->position.v;
+
+		count = 1;
+
+		do {
+
+			if (boid != sp) { // not itself
+				if(abs(boid->position.h - sp->position.h) < kMaxdistance &&
+					abs(boid->position.v - sp->position.v) < kMaxdistance
+				){
+					// average position
+					count++;
+					avgPosH += boid->position.h;
+					avgPosV += boid->position.v;
+				}
+			}
+			boid = boid->next;
+		} while (boid != NULL);
+
+		if (count > 1) {
+			tp_h = (avgPosH / count) - sp->position.h;
+			tp_v = (avgPosV / count) - sp->position.v;
+			v = Normalize(SetVector(tp_v, 0, tp_h));
+
+			if (abs(tp_h) > kMindistance && abs(tp_v) > kMindistance) {
+				sp->speed.v = v.x;
+				sp->speed.h = v.z;
+			} else {
+				sp->speed.v = 0;
+				sp->speed.h = 0;
+			}
+
+		}
+
+		sp = sp->next;
+	} while (sp != NULL);
+
+
 }
 
 // Drawing routine
@@ -100,9 +156,12 @@ void Init()
 	dogFace = GetFace("bilder/dog.tga");			 // En hund
 	foodFace = GetFace("bilder/mat.tga");			 // Mat
 
-	NewSprite(sheepFace, 100, 200, 1, 1);
+	NewSprite(sheepFace, 400, 400, 1, 1);
 	NewSprite(sheepFace, 200, 100, 1.5, -1);
-	NewSprite(sheepFace, 250, 200, -1, 1.5);
+	NewSprite(sheepFace, 700, 500, -1, 1.5);
+	NewSprite(sheepFace, 50, 50, 2, 1.5);
+	NewSprite(sheepFace, 175, 90, -0.5, 1.5);
+	NewSprite(dogFace, 20, 10, 1.5, -1);
 }
 
 int main(int argc, char **argv)
